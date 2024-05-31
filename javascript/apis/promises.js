8512a7a831e985d0898c9787fb9e5b866c7003c7
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-05-31 10:43:29 +0530

    fetch apis

--- javascript/apis/promises.js
+++ javascript/apis/promises.js
@@ -12,70 +12,70 @@ const fetch = require("node-fetch");
 
 //Creating Promises by using variable
 
-// const promiseone = new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log("promiseone created");
-//     resolve();
-//   }, 2000);
-// });
+const promiseone = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("promiseone created");
+    resolve();
+  }, 2000);
+});
 
 // then is connected to resolve but we have to connect it explicitly
 // by calling resolve function in promise
-// promiseone.then(function () {
-//   console.log("promises then resolved ");
-// });
-// promiseone.catch(function () {
-//   console.log("promises catch resolved ");
-// });
+promiseone.then(function () {
+  console.log("promises then resolved ");
+});
+promiseone.catch(function () {
+  console.log("promises catch resolved ");
+});
 
 // without storing into a variable
-// new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log("promiseTwo created");
-//     resolve();
-//   }, 1000);
-// }).then(function () {
-//   console.log("promisesTWo  resolved ");
-// });
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("promiseTwo created");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("promisesTWo  resolved ");
+});
 
-// new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log("data  created and sending");
-//     resolve({ user: "prachi", email: "prachips2621@gmail.com" }); // in curlybraces we are passing object values
-//   }, 1000);
-// }).then(function (user) {
-//   // here we are taking that object its like
-//   // user={
-//   //     user:"prachi",
-//   //     email:"prachips2621@gmail.com"
-//   // }
-//   console.log(user);
-// });
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("data  created and sending");
+    resolve({ user: "prachi", email: "prachips2621@gmail.com" }); // in curlybraces we are passing object values
+  }, 1000);
+}).then(function (user) {
+  // here we are taking that object its like
+  // user={
+  //     user:"prachi",
+  //     email:"prachips2621@gmail.com"
+  // }
+  console.log(user);
+});
 
-// new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log("PromiseFour Created");
-//     let error = false;
-//     if (!error) {
-//       resolve({ user: "prachi", email: "prachips2621@gmail.com" });
-//     } else {
-//       reject("something went wrong");
-//     }
-//   }, 1000);
-// })
-//   .then(function (user) {
-//     console.log(user);
-//     return user.email;
-//   })
-//   .then(function (email) {
-//     console.log(`value in THEN block : ${email}`);
-//   })
-//   .catch(function (msg) {
-//     console.log(`catch block :${msg}`);
-//   })
-//   .finally(function () {
-//     console.log("finally block : promise is either rejected or resolved");
-//   });
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("PromiseFour Created");
+    let error = false;
+    if (!error) {
+      resolve({ user: "prachi", email: "prachips2621@gmail.com" });
+    } else {
+      reject("something went wrong");
+    }
+  }, 1000);
+})
+  .then(function (user) {
+    console.log(user);
+    return user.email;
+  })
+  .then(function (email) {
+    console.log(`value in THEN block : ${email}`);
+  })
+  .catch(function (msg) {
+    console.log(`catch block :${msg}`);
+  })
+  .finally(function () {
+    console.log("finally block : promise is either rejected or resolved");
+  });
 
 // Async await : it waits for the function to gets completed then only moves furthir
 
@@ -85,41 +85,41 @@ const fetch = require("node-fetch");
 // this is just another way of handling asynchronous task like network calls etc
 // resolving promise using async await not then catch
 
-// const promiseFive = new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log("PromiseFive Created");
-//     let error = false;
-//     if (!error) {
-//       resolve({ user: "js", password: "javascript" });
-//     } else {
-//       reject(" ERROR : something went wrong");
-//     }
-//   }, 1000);
-// });
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("PromiseFive Created");
+    let error = false;
+    if (!error) {
+      resolve({ user: "js", password: "javascript" });
+    } else {
+      reject(" ERROR : something went wrong");
+    }
+  }, 1000);
+});
 
 // here there is no catch so we have to define try catch block
-// async function getData() {
-//   try {
-//     const response = await promiseFive; // its an object not function promiseFive() --> wrong
-//     console.log(response);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// getData();npm install node-fetch
+async function getData() {
+  try {
+    const response = await promiseFive; // its an object not function promiseFive() --> wrong
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(); //npm install node-fetch
 
-// async function getusers() {
-//   try {
-//     // ReferenceError: fetch is not defined you have to install library npm install node-fetch
+async function getusers() {
+  try {
+    // ReferenceError: fetch is not defined you have to install library npm install node-fetch
 
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//     const data = await response.json(); // converting from string to json also takes time so thats why await
-//     console.log(data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// getusers();
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json(); // converting from string to json also takes time so thats why await
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getusers();
 
 // fetch using promises
 fetch("https://jsonplaceholder.typicode.com/users")
