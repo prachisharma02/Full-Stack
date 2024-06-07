70d246d296b9d9accf58a539380da7ec2ace80c2
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-06-07 11:23:35 +0530

    props

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+
 function App() {
-  return <>Learn React</>;
+  const username = "prachi";
+
+  return (
+    <>
+      Learn React with {username};{" "}
+      {/* here we can only write evaluated expression not evaluations like if else etc 
+      because react syntax does not support this so react syntax is 
+       const reactelement = React.createElement(
+    "a",
+    {
+      href: "https://www.google.com/",
+    },
+    "click"
+    if-else // it will not work cuz its a js object only reacll js obj can we write if else condition in js objects no 
+    thats why we can only write evaluated expression
+  ); so here if we write like if else
+   */}
+    </>
+  );
 }
 
 export default App;
--- React/vite-react/src/main.jsx
+++ React/vite-react/src/main.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+const element = {
+  type: "a",
+  props: {
+    href: "https://www.google.com/",
+    target: "_blank",
+  },
+  value: "click on the page",
+};
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// so here we get to know that there is a proper syntax of react that how it compiles the html code in react
+//and render it the above code will not work because syntax is wronng in custom react we created our own syntax
+//thats why its working
+
+const viteelement = (
+  <a href="https://www.google.com/" target="_blank">
+    google
+  </a>
 );
+const username = "prachi";
+// now if we want to create through react internal syntax then
+const reactelement = React.createElement(
+  // react.createElement is injected by babel
+  "a",
+  {
+    href: "https://www.google.com/",
+  },
+  "click",
+  username
+);
+
+// at the end of the day react converts every code in a tree called as dom tree
+ReactDOM.createRoot(document.getElementById("root")).render(reactelement); // to c
